b45e1cc11a94b3c7b5ede4ccd31ade047a80172b
ismaelfari1996 <abel_fari@hotmail.com>
2025-12-23 19:00:38 -0500

    update

--- monitory/main.py
+++ monitory/main.py
@@ -1,13 +1,7 @@
 from modules.nodes.healthCheck import check_nodes
 from modules.nodes.resources import get_node_resources
-from modules.pods.healthCheck import check_pod_health
-from modules.services.healthCheckTCP import get_services_health
-from modules.utils.utils import print_table
 from modules.mail.sendmail import send_alert_email
-from modules.utils.pdfreport import generate_pdf_report
 from modules.utils.pdfgenerator import PDFReport
-from modules.storage.nfsfilesystem import get_nfs_filesystem_usage
-from modules.storage.getstorageusage import get_nfs_usage_by_pv
 from datetime import datetime
 
 
@@ -43,43 +37,7 @@ def main():
         resource_rows
     )
     
-    # Check pv usaged
-    print("Monitoring Persisten Volume used...\n")
-    nfs_rows, nfs_issues = get_nfs_usage_by_pv("/mnt/nfs-server/")
-    pdf.add_section("PERSISTEN VOLUME USED")
-    pdf.add_table(
-        ["PV", "folder","Used", "Size", "Used %", "Estatus"],
-        nfs_rows
-    )
     
-    # NFS Check
-    print ("NFS Cheking ...\n")
-    fs = get_nfs_filesystem_usage("/mnt/nfs-server/")
-    pdf.add_section("NFS SERVER STATUS")
-    pdf.add_table(
-        ["FileSystem", "Size","Used", "Available", "Used %"],
-        [[fs["filesystem"],fs["size"],fs["used"],fs["available"],fs["use_pct"]]]
-    )
-
-    # Health check Pods
-    print("Monitoring pods health...\n")
-    pod_rows, pod_issues = check_pod_health()
-    
-    pdf.add_section("PODS UNHEALTHY")
-    pdf.add_table(
-        ["Namespace", "Pod", "Status", "Error Code", "Details"],
-        pod_rows
-    )
-    
-
-    #Health check svc
-    print("Monitoring service health...\n")
-    svc_rows, svc_err = get_services_health()
-    pdf.add_section("SERVICE HEALTH STATUS")
-    pdf.add_table(
-        ["Namespace", "Service", "Port", "Estatus", "Details"],
-        svc_rows
-    )
     pdf.save()
 
     
@@ -93,11 +51,10 @@ def main():
         )
     
     message += f"Nodes: {'NOK' if node_issues else 'OK'}\n"
-    message += f"Pods: {'NOK' if pod_issues else 'OK'}\n"
-    message += f"Services: {'NOK' if svc_err else 'OK'}\n"
 
 
-    if node_issues or resource_issues or svc_err or pod_issues:
+
+    if node_issues or resource_issues:
         message+=(
             "Cluster errors were detected; the PDF report is attached below\n\n"
             "This monitoring was performed automatically; therefore, discrepancies may occur. "
